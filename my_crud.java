4ce585266c10979ffc0abb6ea13f804b77af122c
ph1l1 <ph1l1@DESKTOP-PL43STN>
2024-07-08 21:42:14 -0500

    Add read feature

--- my_crud.java
+++ my_crud.java
@@ -19,19 +19,48 @@ public class my_crud {
 			
 			Scanner scan = new Scanner(System.in);
 			
-			//Add User
-			System.out.print("Enter user name: ");
-			String name = scan.nextLine();
+			System.out.println("1. Add User");
+			System.out.println("2. Read User");
 			
-			System.out.print("Enter user email: ");
-			String email = scan.nextLine();
+			System.out.print("Enter Choice: ");
+			String choice = scan.nextLine();
 			
-			System.out.print("Enter identification number: ");
-			String idn = scan.nextLine();
-			
-			query = "INSERT INTO users (name, email, idn) VALUES ('" + name + "', '" + email + "', '" + idn +"')";
-			
-			stmt.executeUpdate(query);
+			switch(choice) {
+			case "1":
+				//Add User
+				System.out.print("Enter user name: ");
+				String name = scan.nextLine();
+				
+				System.out.print("Enter user email: ");
+				String email = scan.nextLine();
+				
+				System.out.print("Enter identification number: ");
+				String idn = scan.nextLine();
+				
+				query = "INSERT INTO users (name, email, idn) VALUES ('" + name + "', '" + email + "', '" + idn +"')";
+				
+				stmt.executeUpdate(query);
+				
+				break;
+			case "2":
+				System.out.print("Enter user id: ");
+				int id = scan.nextInt();
+				
+				query = "SELECT * FROM users WHERE ID = " + id;
+				
+				ResultSet rs = stmt.executeQuery(query);
+				
+				if (rs.next()) {
+					System.out.println("ID: " +  rs.getInt("id"));
+					System.out.println("Name: " +  rs.getString("name"));
+					System.out.println("Email: " +  rs.getString("email"));
+					System.out.println("Indentification number: " +  rs.getString("idn"));
+				} else {
+					System.out.println("User with id = " + id + " not found");
+				}
+				
+				break;
+			}
 			
 			scan.close();
 			stmt.close();
